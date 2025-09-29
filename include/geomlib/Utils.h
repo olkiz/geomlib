25aa2e0733685cb7a55b9cb1fb5307c6c629f1d3
olkiz <olkiz@proton.me>
2025-09-29 23:32:32 +0300

    Fixed code style

--- include/geomlib/Utils.h
+++ include/geomlib/Utils.h
@@ -5,16 +5,16 @@
 
 namespace geomlib
 {
-    template<class A, class B>
-    bool is_equal(A first, B second) noexcept
+    template <class A, class B>
+    bool is_equal( A first, B second ) noexcept
     {
         return first == second;
     }
 
-    template<class A, class B>
-    requires std::is_floating_point_v<A> && std::is_floating_point_v<B>
-    bool is_equal(A first, B second) noexcept
+    template <class A, class B>
+        requires std::is_floating_point_v<A> && std::is_floating_point_v<B>
+    bool is_equal( A first, B second ) noexcept
     {
-        return std::abs(first - second) < std::numeric_limits<std::common_type_t<A,B>>::epsilon();
+        return std::abs( first - second ) < std::numeric_limits<std::common_type_t<A, B>>::epsilon();
     }
 } // namespace geomlib
